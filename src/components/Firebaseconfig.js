4b9bb16c09bc0e2678235fe4eb7a418fd2f72901
tusharbali2109 <116349676+tusharbali2109@users.noreply.github.com>
2023-07-22 11:32:09 -0700

    add build

--- src/components/Firebaseconfig.js
+++ src/components/Firebaseconfig.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDKS91jM7wpjbDPZuzUqRgTP3Kcz22rzVA",
   authDomain: "netflix-e4349.firebaseapp.com",
@@ -16,7 +12,7 @@ const firebaseConfig = {
   measurementId: "G-KVLK8EV71N",
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
